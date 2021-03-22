857c959f601766339498d497bec9e61377ba785d
yang-daniel <yang.dan@husky.neu.edu>
2021-03-21 21:08:38 -0400

    A5.1 added env variables

--- src/services/widget-service.js
+++ src/services/widget-service.js
@@ -1,5 +1,5 @@
-const TOPICS_URL = "http://localhost:8080/api/topics";
-const WIDGETS_URL = "http://localhost:8080/api/widgets";
+const TOPICS_URL = process.env.REACT_APP_TOPIC_URL;
+const WIDGETS_URL = process.env.REACT_APP_WIDGET_URL;
 
 export const createWidget = (topicId, widget) =>
     fetch(`${TOPICS_URL}/${topicId}/widgets`, {
